0ecbc22759dcbbb31d3f511c78e1b08585a05267
user <user@srv-endpoint-01.hmsm.local>
2023-09-15 18:11:51 -0300

    Addition of Note model logic

--- database/migrations/2023_09_15_210137_create_notes_table.php
+++ database/migrations/2023_09_15_210137_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('hash');
+            $table->longText('data');
+            $table->timestamp('destruction_time')->nullable();
             $table->timestamps();
         });
     }
